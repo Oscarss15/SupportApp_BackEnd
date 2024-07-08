a27c2172972fcd4d81d24fb43d9a019e80274e94
Oscar <oscar15-91@hotmail.com>
2024-07-08 20:53:50 +0200

    añadido estructura

---
+++ src/main/java/dev/oscar/supportApp/SupportAppApplication.java
@@ -0,0 +1,13 @@
+package dev.oscar.supportApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/oscar/supportApp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oscar.supportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
